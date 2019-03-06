3d58de86a6d26ffb08915bcd7fa304854d5216e0
Andriy Sorochan <xai1983kbu@gmail.com>
2019-03-06 12:52:35 +0200

    Section 3 - Social Login with Google OAuth 2.0

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
+const ME_WUERY = `
+{
+  me {
+    _id
+    name
+    email
+    picture
+  }
+}`;
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = async googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken }
+    });
+    const data = await client.request(ME_WUERY);
+    console.log({ data })
+  };
+
+  return (
+    <GoogleLogin
+      clientId="272781104722-9a3kda10svet1a9i2lbc14plicvd3gf2.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
