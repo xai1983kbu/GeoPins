e928d6d70fa103e853c3f3e9807ce13820977864
Andriy Sorochan <xai1983kbu@gmail.com>
2019-03-07 22:54:06 +0200

    Section 8 - Creating Blog Area / Adding Draft Pins

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
@@ -6,6 +6,8 @@ import { withStyles } from "@material-ui/core/styles";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
 import PinIcon from './PinIcon';
+import Blog from './Blog';
+import Context from '../context';
 
 const INITIAL_VIEWPORT = {
   latitude: 37.7577,
@@ -14,6 +16,7 @@ const INITIAL_VIEWPORT = {
 };
 
 const Map = ({ classes }) => {
+  const { state, dispatch } = useContext(Context);
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
   const [userPosition, setUserPosition] = useState(null);
   useEffect(() => {
@@ -30,6 +33,18 @@ const Map = ({ classes }) => {
     }
   };
 
+  const handleMapClick = ({ lngLat, leftButton }) => {
+    if (!leftButton) return;
+    if (!state.draft) {
+      dispatch({ type: "CREATE_DRAFT" })
+    }
+    const [longitude, latitude] = lngLat;
+    dispatch({
+      type: "UPDATE_DRAFT_LOCATION",
+      payload: { longitude, latitude }
+    })
+  };
+
   return (
     <div className={classes.root}>
       <ReactMapGL
@@ -38,6 +53,7 @@ const Map = ({ classes }) => {
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxApiAccessToken="pk.eyJ1IjoibWFyaW9maW9yZSIsImEiOiJjanN5eDVueGUwMzk2M3lwMmprdzFuM3d2In0.GPUPVL13Z3kFJp1s2l5MAQ"
         onViewportChange={newViewport => setViewport(newViewport)}
+        onClick={handleMapClick}
         {...viewport}
       >
       {/* Navigation Control */}
@@ -58,7 +74,25 @@ const Map = ({ classes }) => {
           <PinIcon size={40} color="red" />
         </Marker>
       )}
+
+      {/* Draft Pin */}
+        {state.draft && (
+          <>
+          <Marker
+            latitude={state.draft.latitude}
+            longitude={state.draft.longitude}
+            // offsetLeft={-19}
+            // offsetTop={-37}
+          >
+            <PinIcon size={40} color="hotpink" />
+          </Marker>
+          </>
+        )}
       </ReactMapGL>
+
+
+      {/* Blog Area to add Pin Content */}
+      <Blog />
     </div>
   );
 };
--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,15 +1,103 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import TextField from "@material-ui/core/TextField";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
-// import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import SaveIcon from "@material-ui/icons/SaveTwoTone";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
+import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
+import ClearIcon from "@material-ui/icons/Clear";
+import SaveIcon from "@material-ui/icons/SaveTwoTone";
+import Login from "../Auth/Login";
+
+import Context from "../../context";
 
 const CreatePin = ({ classes }) => {
-  return <div>CreatePin</div>;
+  const { dispatch } = useContext(Context);
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleDeleteDraft = () => {
+    setTitle("");
+    setImage("");
+    setContent("");
+    dispatch({ type: "DELETE_DRAFT" })
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log({ title, image, content })
+  };
+
+  return (
+    <form className={classes.form}>
+      <Typography
+        className={classes.alignCenter}
+        component="h2"
+        variant="h4"
+        color="secondary"
+      >
+        <LandscapeIcon className={classes.iconLarge} /> Pin Location
+      </Typography>
+      <div>
+        <TextField name="title" label="Title" placeholder="Insert pin title"
+          onChange={e => setTitle(e.target.value)}
+        />
+        <input
+          accept="image/*"
+          id="image"
+          type="file"
+          className={classes.input}
+          onChange={e => setImage(e.target.files[0])}
+        />
+        <label htmlFor="image">
+          <Button
+            style={{ color: image && "green" }}
+            component="span"
+            size="small"
+            className={classes.button}
+          >
+            <AddAPhotoIcon />
+          </Button>
+        </label>
+      </div>
+      <div className={classes.contentField}>
+        <TextField
+          name="content"
+          label="Content"
+          multiline
+          rows="6"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          onChange={e => setContent(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          onClick={handleDeleteDraft}
+        >
+          <ClearIcon className={classes.leftIcon} />
+          Discard
+        </Button>
+        <Button
+          type="submit"
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+          disabled={!title.trim() || !content.trim() || !image}
+          onClick={handleSubmit}
+        >
+          Submit
+          <SaveIcon className={classes.rightIcon} />
+        </Button>
+
+      </div>
+    </form>
+  );
 };
 
 const styles = theme => ({
