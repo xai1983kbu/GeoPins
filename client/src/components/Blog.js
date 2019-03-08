6c77db4708e4dabf9f043bb417dd8521de149407
Andriy Sorochan <xai1983kbu@gmail.com>
2019-03-08 20:09:57 +0200

    Section 15 - Displaying Pin Content

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -5,18 +5,21 @@ import { Paper } from "@material-ui/core";
 import Context from '../context';
 import  NoContent from './Pin/NoContent';
 import CreatePin from './Pin/CreatePin';
+import PinContent from './Pin/PinContent';
 
 const Blog = ({ classes }) => {
   const { state } = useContext(Context);
-  const { draft } = state;
+  const { draft, currentPin } = state;
 
   let BlogContent;
-  if (!draft) {
+  if (!draft && !currentPin) {
     // nocontent
     BlogContent = NoContent
-  } else if (draft) {
+  } else if (draft && !currentPin) {
     // create pin
     BlogContent = CreatePin
+  } else if (!draft && currentPin) {
+    BlogContent = PinContent
   }
   return (
     <Paper className={classes.root}>
