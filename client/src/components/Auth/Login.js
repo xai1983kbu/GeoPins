2ae303c19a603191d308852422301e4105e21550
Andriy Sorochan <xai1983kbu@gmail.com>
2019-03-08 22:59:56 +0200

    Section 17 - Handling Expired Auth Token Errors

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -29,7 +29,8 @@ const Login = ({ classes }) => {
   };
 
   const onFailure = err => {
-    console.error("Error logging in", err)
+    console.error("Error logging in", err);
+    dispatch({ type: "IS_LOGGED_IN", payload: false });
   };
 
   return (
